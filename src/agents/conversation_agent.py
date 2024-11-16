58f9140017640903489066f433c5fe5f4c8c036c
liwj1204 <164465083+liwj1204@users.noreply.github.com>
2024-11-16 23:02:56 +0800

    update conversation_prompt.txt

--- src/agents/conversation_agent.py
+++ src/agents/conversation_agent.py
@@ -47,7 +47,7 @@ class ConversationAgent:
 
         # 初始化 ChatOllama 模型，配置模型参数
         self.chatbot = self.prompt | ChatOllama(
-            model="llama3.1:8b-instruct-q8_0",  # 使用的模型名称
+            model="llama3.1:8b-instruct-q4_0",  # 使用的模型名称
             max_tokens=8192,  # 最大生成的token数
             temperature=0.8,  # 生成文本的随机性
         )
